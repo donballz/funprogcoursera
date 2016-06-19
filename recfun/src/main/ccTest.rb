ecd847c1504b5a44e8dfe5fb3c24aa36420f493e
Donald Armstead <donald.armstead@gmail.com>
2016-06-18 20:45:36 -0400

    debug: add additional test case

--- recfun/src/main/ccTest.rb
+++ recfun/src/main/ccTest.rb
@@ -2,22 +2,26 @@
 # coins.head = coins[0]
 # coins.tail = coins[1..-1]
 
-def checkOnePath(money, coins, track)
+DEBUG = true
+
+def checkOnePath(money, coins, track, path)
+	puts path if DEBUG
     if (not coins.any?) or money < 0
-    	puts "path: #{track}; term at 0 for lack of coins" unless coins.any?
-    	puts "path: #{track}; term at 0 for lack of money" if money < 0
+    	puts "path: #{track}; term at 0 for lack of coins" if not coins.any? and DEBUG
+    	puts "path: #{track}; term at 0 for lack of money" if money < 0 and DEBUG
     	0
-    elsif (money == 0) 
-    	puts "path: #{track}; term at 1 with coins #{coins}"
-    	1
-    elsif (money >= coins[0])
+    elsif (money == coins[0]) 
     	track.push(coins[0])
-    	puts "track add #{coins[0]} with money #{money} for result #{track}"
-      	checkOnePath(money - coins[0], coins, track) +
-      	checkOnePath(money - coins[0], coins[1..-1], track)
+    	puts "path: #{track}; term at 1 with coins #{coins}" if DEBUG
+    	1 
+    elsif (money > coins[0])
+    	track.push(coins[0])
+    	puts "track add #{coins[0]} with money #{money} for result #{track}" if DEBUG
+      	checkOnePath(money - coins[0], coins, track.reverse.reverse, path + "A") +
+      	checkOnePath(money - coins[0], coins[1..-1], track.reverse.reverse, path + "B")
     else 
-    	puts "coin #{coins[0]} too large for money #{money}"
-    	checkOnePath(money, coins[1..-1], track)
+    	puts "coin #{coins[0]} too large for money #{money} #{track}" if DEBUG
+    	checkOnePath(money, coins[1..-1], track.reverse.reverse, path + "C")
     end
 end
     
@@ -25,7 +29,7 @@ def checkAll(count, money, coins)
     if not coins.any?
     	count
     else 
-    	checkAll(count + checkOnePath(money, coins, []), money, coins[1..-1])
+    	checkAll(count + checkOnePath(money, coins, [], "#{coins[0]}"), money, coins[1..-1])
     end
 end
     
@@ -33,4 +37,7 @@ def countChange(money, coins)
   checkAll(0, money, coins)
 end
 
-puts countChange(4, [1,2])
+#puts countChange(4, [1,2]) # 3
+puts countChange(4, [1,2,3]) # 4
+#puts countChange(300,[5,10,20,50,100,200,500]) # 1022
+#puts countChange(300,[500,5,50,100,20,200,10]) # 1022
