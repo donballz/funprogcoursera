5b32bff8802c2ae50cfe1b0a7168f6ebb941ed2c
Donald Armstead <donald.armstead@gmail.com>
2016-06-19 20:38:20 -0400

    test: failed attempt

--- recfun/src/main/ccTest2.rb
+++ recfun/src/main/ccTest2.rb
@@ -2,34 +2,31 @@
 # coins.head = coins[0]
 # coins.tail = coins[1..-1]
 
-DEBUG = false
-COINS = [1,2,3]
+DEBUG = true
+COINS = [1,2]
 
-def innerLoop(money, innerCoins)
+def myloop(money, innerCoins, acc, track)
+	puts "#{track}" if DEBUG
+	puts "money: #{money}; coin: #{innerCoins[0]}" if DEBUG
     if (not innerCoins.any?) or money < 0 
-    	puts "path: #{track}; term at 0 for lack of coins" if not innerCoins.any? and DEBUG
-    	puts "path: #{track}; term at 0 for lack of money" if money < 0 and DEBUG
+    	puts "term at 0 for lack of coins" if (not innerCoins.any?) and DEBUG
+    	puts "term at 0 for lack of money" if money < 0 and DEBUG
     	0
     elsif (money == 0) 
-    	1
+    	puts "term at 1" if DEBUG
+    	acc + 1
     else 
-    	innerLoop(money - innerCoins[0], innerCoins[1..-1])
-    end
-end
-
-def outerLoop(money, innerCoins, acc)
-    if (not innerCoins.any?) 
-    	acc
-    else 
-    	outerLoop(money, innerCoins[1..-1], acc + innerLoop(money, COINS))
+    	track.push({money => innerCoins[0]})
+    	myloop(money - innerCoins[0], innerCoins[1..-1], acc, track.reverse.reverse) +
+    	myloop(money - innerCoins[0], innerCoins, acc, track.reverse.reverse)
     end
 end
 
 def countChange(money, coins)
-  outerLoop(money, coins, 0)
+  myloop(money, coins, 0, [])
 end
 
-#puts countChange(4, [1,2]) # 3
-puts countChange(4, COINS) # 4
+puts countChange(4, [1,2]) # 3
+#puts countChange(4, COINS) # 4
 #puts countChange(300,[5,10,20,50,100,200,500]) # 1022
 #puts countChange(300,[500,5,50,100,20,200,10]) # 1022
