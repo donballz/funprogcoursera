c4c8f570d529af32acf896c9fc0d2e197b201f2a
Donald Armstead <donald.armstead@gmail.com>
2016-06-28 01:11:35 -0400

    feat: week3 submission passed 100%

--- objsets/src/main/scala/objsets/TweetSet.scala
+++ objsets/src/main/scala/objsets/TweetSet.scala
@@ -136,14 +136,17 @@ object GoogleVsApple {
   val google = List("android", "Android", "galaxy", "Galaxy", "nexus", "Nexus")
   val apple = List("ios", "iOS", "iphone", "iPhone", "ipad", "iPad")
 
-  lazy val googleTweets: TweetSet = ???
-  lazy val appleTweets: TweetSet = ???
+  def assoc(words: List[String]): (Tweet) => Boolean =
+    (tw: Tweet) => words.exists((word: String) => tw.text.contains(word))
+
+  lazy val googleTweets: TweetSet = allTweets.filter(assoc(google))
+  lazy val appleTweets: TweetSet = allTweets.filter(assoc(apple))
   
   /**
    * A list of all tweets mentioning a keyword from either apple or google,
    * sorted by the number of retweets.
    */
-     lazy val trending: TweetList = ???
+  lazy val trending: TweetList = (googleTweets union appleTweets).descendingByRetweet
   }
 
 object Main extends App {
